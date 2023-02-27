3d9ad2bdab005253eaa159ce9a89c24b07c5383c
Brett Hughes <twofivinclimber@gmail.com>
2023-02-26 23:32:44 -0600

    pushing to save

--- datafix.sql
+++ datafix.sql
@@ -1,3 +1,10 @@
+UPDATE scoped_api_image
+SET image = "https://res.cloudinary.com/twofiveclimb/image/upload/v1665604523/mad-app/le21h3jowmdjqcziylly.jpg"
+WHERE id = 2
+
 UPDATE scoped_api_crew
-SET accepted = Null
-WHERE uid_id = 1
+SET accepted = null
+WHERE id = 1
+
+DELETE FROM scoped_api_user
+WHERE id = 8
