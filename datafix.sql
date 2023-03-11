bdba8482e70332534c311bfbc204cf43fc0a4db4
Brett Hughes <twofivinclimber@gmail.com>
2023-03-11 15:48:34 -0600

    added fixture command

--- datafix.sql
+++ datafix.sql
@@ -3,8 +3,8 @@ SET image = "https://res.cloudinary.com/twofiveclimb/image/upload/v1665604523/ma
 WHERE id = 2
 
 UPDATE scoped_api_crew
-SET accepted = null
-WHERE id = 59
+SET uid_id = 1
+WHERE id = 1
 
 DELETE FROM scoped_api_crew
 WHERE id = 13
